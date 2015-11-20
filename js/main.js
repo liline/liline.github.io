0b62084d1a76e8b3274b71d5023876fdcd317669
Liline <purewhiteraven@gmail.com>
2015-11-19 22:03:57 -0800

    Change hamburgoo menu and add fancy cog

--- js/main.js
+++ js/main.js
@@ -18,5 +18,12 @@
           }
         }
       });
+
+      /* Collpase hamburgoo menu after clickin' */
+      $(document).ready(function () {
+        $(".navbar-nav li a").click(function(event) {
+          $(".navbar-collapse").collapse('hide');
+        });
+      });
     });
 
